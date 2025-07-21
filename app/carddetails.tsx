16eb964c12b4fa9da2c37919efb59d157a72d5d2
ashishpaudel123 <ashishpaudel206058@gmail.com>
2025-07-21 14:31:51 +0545

    Add CardDetailsScreen component to display card details

---
+++ app/carddetails.tsx
@@ -0,0 +1,18 @@
+import CardDetails from '@/components/CardDetails';
+import { useLocalSearchParams } from 'expo-router';
+import React from 'react';
+import { View } from 'react-native';
+
+export default function CardDetailsScreen() {
+  const params = useLocalSearchParams();
+  // params: { title, poster, description }
+  return (
+    <View style={{ flex: 1, backgroundColor: '#181818', justifyContent: 'center', alignItems: 'center' }}>
+      <CardDetails
+        title={params.title as string}
+        poster={params.poster}
+        description={params.description as string}
+      />
+    </View>
+  );
+}
