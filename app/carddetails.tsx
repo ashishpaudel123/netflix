09a450921ed9fa9e79f6c25a718b89d370ba1034
ashishpaudel123 <ashishpaudel206058@gmail.com>
2025-07-26 09:53:24 +0545

    feat: Update CardDetailsScreen to include additional parameters for enhanced movie details

--- app/carddetails.tsx
+++ app/carddetails.tsx
@@ -5,15 +5,21 @@ import { View } from 'react-native';
 
 export default function CardDetailsScreen() {
   const params = useLocalSearchParams();
-  // params: { title, poster, description }
+  // params: { title, poster, description, subtitle, year, duration, cast, rating, matchPercentage, videoSource }
   return (
     <View style={{ flex: 1, backgroundColor: '#181818' }}>
       <CardDetails
-        id={params.id as number}
+        id={params.id as any}
         title={params.title as string}
         poster={params.poster}
         description={params.description as string}
-        onPlay={() => router.push({ pathname: "/playvideo", params })} // Pass the VideoPlayer component directly
+        subtitle={params.subtitle as string}
+        year={params.year as number | string}
+        duration={params.duration as string}
+        cast={params.cast as string}
+        rating={params.rating as string}
+        category={params.category as string}
+        onPlay={() => router.push({ pathname: "/playvideo", params })}
       />
     </View>
   );
